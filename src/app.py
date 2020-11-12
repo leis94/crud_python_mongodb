dc23d432316f0dd420a0475d4c617f6e2b17786a
Daniel <bukaro.dany@gmail.com>
2020-11-11 23:22:03 -0500

    Get a task and list of tasks and adding route

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify, Response
 from flask_pymongo import PyMongo
+from bson import json_util
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 app.config['MONGO_URI']='mongodb+srv://admin:admin123#@cluster0.p49qk.mongodb.net/flask_mongodb?retryWrites=true&w=majority'
@@ -26,6 +29,20 @@ def create_task():
         return response
     else:
         return not found()
+
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    tasks = mongo.db.tasks.find()
+    response = json_util.dumps(tasks)
+    return Response(response, mimetype='application/json')
+
+
+@app.route('/tasks/<id>', methods=['GET'])
+def get_task(id):
+    task = mongo.db.tasks.find_one({'_id': ObjectId(id)})
+    response = json_util.dumps(task)
+    return Response(response, mimetype='application/json')
 
 
 @app.errorhandler(404)
